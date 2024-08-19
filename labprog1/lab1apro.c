4eea5848510de2539dcc3ad975a90ed25ff03bce
HLG Sagar <gangasagarhl1112@gmail.com>
2024-08-19 18:25:42 +0530

    Uploaded on Aug19th 66:25pm
    Lab 1program

---
+++ labprog1/lab1apro.c
@@ -0,0 +1,77 @@
+/*
+
+ROLL NUMBER: MT2024048
+NAME: GANGASAGER HL
+a. softlink(symlink system call)
+b. hard link(link system call)
+*/
+
+// Creating_softlink using sytem call
+
+#include<stdio.h>//normal printf and scanf
+#include<unistd.h>// for creating links
+#include<stdlib.h>// for system() library function 
+
+//softlink 
+void softy()
+{
+// This function called from the main , used to create the soft link of the function
+
+const char *hlgs_target_file = "simple.txt";
+const char *hlgs_soft_destination = "soft_function_call";
+
+//symlink is available in unistd
+	if(symlink(hlgs_target_file,hlgs_soft_destination )==-1)
+	{
+		perror("Symlink unable to  create:\n");
+		exit(EXIT_FAILURE);
+		
+
+	}
+	
+
+}
+
+//hardlink
+void hardy()
+{
+
+// This function called from the main , used to create the hard link of the function
+
+const char *hlgs_target_file = "simple.txt";
+const char *hlgs_soft_destination = "hard_function_call";
+
+//link is available in unistd, and create the hard link of the file mentioned above.
+	if(link(hlgs_target_file,hlgs_soft_destination )==-1)
+	{
+		perror("Symlink unable to  create:\n");
+		exit(EXIT_FAILURE);
+		
+
+	}
+
+}
+
+
+
+
+//main function call
+int main(){
+
+int flag;
+printf("\nEnter 1 to create softlink using system call\nEnter 2 to create hardlink using system call, for the files entered statically");
+
+scanf("%d",&flag);
+printf("\n\nList of files  in this directory before calling system calls:\n");
+system("ls -l");
+printf("\n");
+
+
+if(flag==1)softy();
+else if(flag==2)hardy();
+
+printf("\n\nList of files  in this directory after calling system calls:\n");
+system("ls -l");
+printf("\n");
+
+}
